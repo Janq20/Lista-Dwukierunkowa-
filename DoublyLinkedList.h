396a87c921297109166a9ce849ae1645b71807ca
Janek W <89817308+Janq20@users.noreply.github.com>
2025-10-20 22:18:35 +0200

    Dodanie funkcji wstawiania elementu na koniec listy

--- DoublyLinkedList.h
+++ DoublyLinkedList.h
@@ -1,11 +1,22 @@
 #include "DoublyLinkedList.h"
 #include <iostream>
 
-void DoublyLinkedList::displayForward() {
-    Node* temp = head;
-    while (temp) {
-        std::cout << temp->data << " ";
-        temp = temp->next;
+void DoublyLinkedList::append(int value) {
+    Node* newNode = new Node(value);
+    if (!head) {
+        head = tail = newNode;
+    } else {
+        tail->next = newNode;
+        newNode->prev = tail;
+        tail = newNode;
     }
-    std::cout << "\n";
+}
+
+DoublyLinkedList::~DoublyLinkedList() {
+    Node* current = head;
+    while (current) {
+        Node* next = current->next;
+        delete current;
+        current = next;
+    }
 }
